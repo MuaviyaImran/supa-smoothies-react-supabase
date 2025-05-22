b7b91f09e5aea897200f31ffcbb50a7e70309677
Muaviyaimran <muaviyaimran1122@gmail.com>
2025-05-22 13:31:06 +0500

    Created Supabase Connection

---
+++ src/config/supabaseClient.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// Check if the environment variables are set
+if (!supabaseUrl) {
+  console.error('Supabase URL is not set in environment variables');
+}
+if (!supabaseKey) {
+  console.error('Supabase Key is not set in environment variables');
+}
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and Key must be set in environment variables');
+}
+// Create a single supabase client for interacting with your database
+const supabase: unknown = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
